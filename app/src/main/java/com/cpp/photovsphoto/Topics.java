9b2191d8e7f44863d90ac3ea360f738b920d5d35
Brandon <bdchan21@gmail.com>
2016-05-21 12:40:28 -0700

    Added code to topics to link to versus screen.

--- app/src/main/java/com/cpp/photovsphoto/Topics.java
+++ app/src/main/java/com/cpp/photovsphoto/Topics.java
@@ -1,12 +1,18 @@
 package com.cpp.photovsphoto;
 //trying to push fragments.
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.cpp.photovsphoto.fragments.fragment_analysis_result;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,11 +64,83 @@ public class Topics extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    Button officeButton;
+    Button kitchenButton;
+    Button foodButton;
+    Button petItemsButton;
+    Button bcGamesButton;
+    Button drinksButton;
+    Button sidesButton;
+    Button dessertsButton;
+    Button roadTripButton;
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_topics, container, false);
+        officeButton = (Button) view.findViewById(R.id.buttonOffice);
+        kitchenButton = (Button) view.findViewById(R.id.buttonKitchen);
+        foodButton = (Button) view.findViewById(R.id.buttonFood);
+        petItemsButton = (Button) view.findViewById(R.id.buttonPetItems);
+        bcGamesButton = (Button) view.findViewById(R.id.buttonBCGames);
+        drinksButton = (Button) view.findViewById(R.id.buttonDrinks);
+        sidesButton = (Button) view.findViewById(R.id.buttonSides);
+        dessertsButton = (Button) view.findViewById(R.id.buttonDesserts);
+        roadTripButton = (Button) view.findViewById(R.id.buttonRoadTrip);
+        officeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        kitchenButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        foodButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        petItemsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        bcGamesButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        drinksButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        sidesButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        dessertsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
+        roadTripButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                WaitForResponse();
+            }
+        });
         return inflater.inflate(R.layout.fragment_topics, container, false);
     }
 
@@ -103,5 +181,14 @@ public class Topics extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void WaitForResponse(){ //inflate new fragment
+        FragmentActivity activity = this.getActivity();
+        activity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, new Versus(), "Versus")
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit();
     }
 }
